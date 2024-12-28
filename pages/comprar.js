cb56c3c2f1881d699094bfba80a911ac2cbd85e9
manuelguirado <manu22.mgb@gmail.com>
2024-12-28 13:37:09 +0100

    finish the project

---
+++ pages/comprar.js
@@ -0,0 +1,12 @@
+const compra = document.getElementById('compra');
+
+const anadir = document.querySelector('#anadir');
+compra.addEventListener('click', () => {
+
+alert('Compra realizada con éxito');
+
+});
+anadir.addEventListener('click', () => {    
+
+    alert('Producto añadido al carrito');
+});
